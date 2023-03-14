8af9b6d00ad70473c690f258f137b24d172c5b69
Zhuoyao Lin <zhuoyao.lin@polytechnique.edu>
2023-03-14 02:55:11 +0100

    CUDA: delet not-used code

--- src/filter_cuda.h
+++ src/filter_cuda.h
@@ -6,10 +6,6 @@
 extern "C" {
 #endif
 
-void apply_gray_filter_gpu(animated_gif *image);
-void apply_sobel_filter_gpu(animated_gif *image);
-void apply_all_filters_gpu(animated_gif *image);
-
 #ifdef __cplusplus
 }
 #endif
